bf1a798204235ddeae58d018dbd79683e7e8df27
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-10-01 14:34:53 +0500

    смотрю git

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux'
-import {BrowserRouter as Router} from 'react-router-dom'
 
-import ErrorBoundry from './components/errorBoundry/errorBoundry';
-import Services from './services/services';
-import {ServicesProvider} from './components/servicesContext/servicesContext';
-import App from './components/app/app'
 
-import store from './store'
 
-const services = new Services();
+class App extends React.Component{
+
+  componentDidCatch() {
+    console.log('err');
+  }
+
+  render(){
+
+    return null
+
+    return(
+      <p>    </p>
+    )
+  }
+};
+
+
+
+
+
+
+
+
+
 
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <ErrorBoundry>
-      <ServicesProvider value={ services }>
-        <Router>
-          <App />
-        </Router>
-      </ServicesProvider>
-    </ErrorBoundry>
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
-
